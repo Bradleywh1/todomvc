dc19775fb1325793f82bb6c8d5c3ee9bddab97fd
Bradley Wheeler <bradley.wh@outlook.com>
2019-06-13 14:24:05 +1200

    remove store from util

--- public/js/app.js
+++ public/js/app.js
@@ -28,19 +28,20 @@ jQuery(function ($) {
 		pluralize: function (count, word) {
 			return count === 1 ? word : word + 's';
 		},
-		store: function (namespace, data) {
-			if (arguments.length > 1) {
-				return localStorage.setItem(namespace, JSON.stringify(data));
-			} else {
-				var store = localStorage.getItem(namespace);
-				return (store && JSON.parse(store)) || [];
-			}
-		}
 	};
+
+	function store(namespace, data) {
+		if (arguments.length > 1) {
+			return localStorage.setItem(namespace, JSON.stringify(data));
+		} else {
+			var store = localStorage.getItem(namespace);
+			return (store && JSON.parse(store)) || [];
+		}
+	}
 
 	var App = {
 		init: function () {
-			this.todos = util.store('todos-jquery');
+			this.todos = store('todos-jquery');
 			this.todoTemplate = Handlebars.compile($('#todo-template').html());
 			this.footerTemplate = Handlebars.compile($('#footer-template').html());
 			bindEvents();
@@ -203,7 +204,7 @@ jQuery(function ($) {
 		$('#toggle-all').prop('checked', getActiveTodos().length === 0);
 		renderFooter();
 		$('#new-todo').focus();
-		util.store('todos-jquery', App.todos);
+		store('todos-jquery', App.todos);
 	}
 
 	App.init();
